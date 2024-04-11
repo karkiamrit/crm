23e1e16bc0107670fdb88243618a603ada7faa8c
iam8uman <me8848you@gmail.com>
2024-04-11 21:12:11 +0545

    Segment content

--- frontend/src/components/leads/row/DrawerContent.tsx
+++ frontend/src/components/leads/row/DrawerContent.tsx
@@ -4,12 +4,13 @@ import { Button } from "@/components/ui/button";
 
 export default function DrawerContentDemo() {
   return (
-    <div className="flex justify-between mx-10">
-      <div className="flex gap-3 ">
+    <div className="flex justify-between mx-10 items-center my-6">
+      <div className="flex gap-3  items-center">
         <div className="bg-transparent text-black">1 Selected</div>
-        <div className="text-primary underline underline-offset-2 underline-primary">Clear All</div>
+        <Button className="text-primary bg-transparent hover:bg-transparent shadow-none   underline underline-offset-2 underline-primary">
+          Clear All
+        </Button>
       </div>
-
       <Button className="w-60 my-2 px-4">Segment</Button>
     </div>
   );
